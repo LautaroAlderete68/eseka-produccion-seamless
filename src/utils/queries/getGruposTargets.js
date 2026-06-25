9ea8f493b9ebb332a9a0a631cd0162a8d6ae21c8
Lautaro Alderete <joblautaro@gmail.com>
2026-06-25 09:56:30 -0300

    Fix getGruposTargets SQL query by defining Produccion and ProdColor CTEs within it to avoid Invalid object name error

--- src/utils/queries/getGruposTargets.js
+++ src/utils/queries/getGruposTargets.js
@@ -2,6 +2,26 @@ const sql = require('mssql');
 
 const getGruposTargets = async (pool) => {
   return pool.request().query(`
+    WITH Produccion AS (
+        SELECT 
+            pm.StyleCode,
+            SUM(pm.Pieces) AS Unidades
+        FROM [dbNautilus].[dbo].[PRODUCTIONS_MONITOR] pm
+        WHERE MONTH(pm.DateRec) = MONTH(GETDATE()) 
+          AND YEAR(pm.DateRec) = YEAR(GETDATE())
+        GROUP BY pm.StyleCode
+    ),
+    ProdColor AS (
+        SELECT 
+            cc.Articulo, 
+            cc.Talle,
+            cc.Color AS ColorId,
+            SUM(p.Unidades) AS Unidades
+        FROM Produccion AS p
+            JOIN APP_COLOR_CODES AS cc
+                ON p.StyleCode = cc.StyleCode
+        GROUP BY cc.Articulo, cc.Talle, cc.Color
+    )
     SELECT 
         CAST(pc.Articulo AS VARCHAR) AS StyleCode,
         pc.Target AS TargetPieces,
